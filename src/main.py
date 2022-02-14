0af70d2d764173a34f0c622e8afa4c2dffab8e7e
Garoze <marcelogaroze@hotmail.com>
2022-02-14 10:27:08 -0300

    feat: Labels functionality
    TODO: Find a better way to deal with labels cause it's not working

--- src/main.py
+++ src/main.py
@@ -53,29 +53,48 @@ def make_instruction(instruction: tuple, value: str = ""):
     """
     match instruction[1]:
         case Modes.IMP:
-            return [instruction[0]]
+            write_out([instruction[0]])
+            return 1
         case Modes.IMM:
             match instruction[2]:
                 case Address.IM8:
-                    return [instruction[0], LSB(value)]
+                    write_out([instruction[0], LSB(value)])
+                    return 2
                 case Address.I16:
-                    return [instruction[0], LSB(value), MSB(value)]
+                    write_out([instruction[0], LSB(value), MSB(value)])
+                    return 3
         case Modes.ABS:
-            return [instruction[0], LSB(value), MSB(value)]
+            write_out([instruction[0], LSB(value), MSB(value)])
+            return 3
+    return 0
 
 def valid_opcode(opcode: str) -> bool:
     return opcode in Opcode_t
 
+def parse_labels():
+    pass
+
+def as_hex(value):
+    return "0x{:04X}".format(value)
+
+LABELS_TABLE = {}
+
 def main():
+    PC = 0x0000
+
     with open('out.bin', "wb") as out:
         out.close()
 
     with open(argv[1]) as f:
-        for line in f:
-            if line[0] in ';.':
-                continue
+        for n, line in enumerate(f):
+            if line[0] in '.':
+                label = line[1:].split(':')[0]
+                if not label in LABELS_TABLE:
+                    LABELS_TABLE[label] = PC
+                else:
+                    print(f"Line: {n} Label -> {label} already defined with the value: {as_hex(LABELS_TABLE[label])}")
 
-            line  = line.replace('\n', '').replace('\r', '')
+            line  = line.replace('\n', '').replace('\r', '').lstrip()
             token = re.split(r'[, ]', line)
 
             opcode = token[0].upper()
@@ -84,14 +103,17 @@ def main():
                 match len(token):
                     case 1:
                         instruction = get_instruction(opcode, Modes.IMP)
-                        b = make_instruction(instruction)
-                        write_out(b)
+                        PC += make_instruction(instruction)
                     case 2:
-                        value = token[1]
-                        mode  = get_mode(value)
-                        instruction = get_instruction(opcode, mode)
-                        b = make_instruction(instruction, value)
-                        write_out(b)
+                        if token[1] in LABELS_TABLE:
+                            value = "$" + str(LABELS_TABLE[token[1]])
+                            instruction = get_instruction(opcode, Modes.ABS)
+                            PC += make_instruction(instruction, value)
+                        else:
+                            value = token[1]
+                            mode  = get_mode(value)
+                            instruction = get_instruction(opcode, mode)
+                            PC += make_instruction(instruction, value)
 
 if __name__ == '__main__':
     if len(argv) < 2:
